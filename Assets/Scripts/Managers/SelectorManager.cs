7fd07e9ec8ae00b0f47bf103b3c22ac95946c2b3
angelhd1999 <angelhd1999@gmail.com>
2020-05-21 20:45:52 +0200

    Some raycasting tests

---
+++ Assets/Scripts/Managers/SelectorManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pose;
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+        //var ray = Camera.main.ScreenPointToRay(pose.GetComponent<TrackingReceiver>().wristR.transform.position); //"original"
+        //var ray = Physics.Raycast(Camera.main.transform.position, pose.GetComponent<TrackingReceiver>().wristR.transform.position);
+        RaycastHit hit;
+        //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        //if (Physics.Raycast(ray, out hit)) //original
+        if (Physics.Raycast(Camera.main.transform.position, pose.GetComponent<TrackingReceiver>().wristR.transform.position, out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(Camera.main.transform.position, pose.GetComponent<TrackingReceiver>().wristR.transform.position * Mathf.Infinity, Color.yellow);
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            { 
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(Camera.main.transform.position, pose.GetComponent<TrackingReceiver>().wristR.transform.position * Mathf.Infinity, Color.white);
+        }
+    }
+}
---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public GameObject physicsManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(Physics.gravity * physicsManager.GetComponent<PhysicsManager>().gravity, ForceMode.Acceleration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
